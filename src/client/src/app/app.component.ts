ecb35145565ba5c78d91b4e770583c702a8b19df
Frederik Traber <ftr@voicepoint.ch>
2019-12-10 22:45:53 +0100

    ui improvments

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,18 @@ export class AppComponent {
   username: '';
   users: any;
   isLoading = false;
+  hasError = false;
 
   constructor(private client: HttpClient) {
     this.load();
+  }
+
+  private handleError() {
+      const self = this;
+      return catchError((error) => {
+        self.hasError = true;
+        throw error;
+      });
   }
 
   private load(): void {
@@ -21,6 +31,7 @@ export class AppComponent {
     const self = this;
     this.client
     .get('https://ntfrex-function-helloworld.azurewebsites.net/api/users')
+    .pipe(this.handleError())
     .subscribe((data) => {
         self.users = data;
         self.isLoading = false;
@@ -33,6 +44,7 @@ export class AppComponent {
     const self = this;
     this.client
       .post('https://ntfrex-function-helloworld.azurewebsites.net/api/user', body)
+      .pipe(this.handleError())
       .subscribe(() => self.load());
   }
 }
